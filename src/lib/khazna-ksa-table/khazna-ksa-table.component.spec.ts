6a3b3b5d0ecb1078e8ed17148fc4e789f17e0061
mohamed-alaa-95 <mohamed.alaa@khazna.app>
2025-04-09 11:21:26 +0200

    feat: clean

--- src/lib/khazna-ksa-table/khazna-ksa-table.component.spec.ts
+++ src/lib/khazna-ksa-table/khazna-ksa-table.component.spec.ts
@@ -1,20 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DateTimeComponent } from './date-time.component';
+import { KhaznaKsaTableComponent } from './khazna-ksa-table.component';
 
-describe('DateTimeComponent', () => {
-  let component: DateTimeComponent;
-  let fixture: ComponentFixture<DateTimeComponent>;
+describe('KhaznaKsaTableComponent', () => {
+  let component: KhaznaKsaTableComponent;
+  let fixture: ComponentFixture<KhaznaKsaTableComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DateTimeComponent ]
+      imports: [KhaznaKsaTableComponent]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DateTimeComponent);
+    fixture = TestBed.createComponent(KhaznaKsaTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
