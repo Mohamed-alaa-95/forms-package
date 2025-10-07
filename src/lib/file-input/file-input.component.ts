2983e09310ae83f9bc46a1e08c16fbb6b298a07a
mohamed-alaa-95 <mohamed.alaa@khazna.app>
2025-10-07 16:25:22 +0300

    feat: add the file type

--- src/lib/file-input/file-input.component.ts
+++ src/lib/file-input/file-input.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   EventEmitter,
   Input,
+  OnChanges,
   Output,
   ViewChild,
 } from '@angular/core';
@@ -15,7 +16,7 @@ import { FileUpload, FileUploadModule } from 'primeng/fileupload';
   templateUrl: './file-input.component.html',
   styleUrl: './file-input.component.css',
 })
-export class FileInputComponent {
+export class FileInputComponent implements OnChanges {
   @ViewChild('uploader') uploader: FileUpload | undefined;
   @Input() form: FormGroup = new FormGroup({});
   @Input() columnConfig: any;
@@ -25,10 +26,15 @@ export class FileInputComponent {
   @Output() onSelectFile = new EventEmitter<any>();
   fileSelected: boolean = false;
   selectedFile: File | null = null;
+
   onUpload(event: any, uploader: FileUpload) {
     this.fileSelected = true;
     this.selectedFile = event.files[0];
-    this.onSelectFile.emit({ event, uploader });
+    this.onSelectFile.emit({
+      event,
+      uploader,
+      onClearFilter: this.onClearFilter,
+    });
   }
 
   downloadSample() {
@@ -40,5 +46,19 @@ export class FileInputComponent {
     this.selectedFile = null;
     this.form.controls[this.columnConfig?.control?.name].setValue(null);
     if (this.uploader) this.uploader.clear();
+  }
+
+  ngOnChanges(): void {
+    this.onClearFilter.subscribe((res) => {
+      if (
+        res['key'] == 'clear all' ||
+        res['key'] === this.columnConfig?.control?.name
+      ) {
+        if (this.uploader) this.uploader.clear();
+        this.form.controls[this.columnConfig?.control?.name].setValue(null);
+        this.fileSelected = false;
+        this.selectedFile = null;
+      }
+    });
   }
 }
